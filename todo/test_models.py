28defcba14f0b51df368d1274ee6aa57503059c0
tWilltp <williamjenkinsfeb@gmail.com>
2023-04-24 16:03:23 +0000

    finished testing, mini project complete

--- todo/test_models.py
+++ todo/test_models.py
@@ -7,3 +7,7 @@ class TestModels(TestCase):
     def test_done_default_to_false(self):
         item = Item.objects.create(name='Test todo item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test todo item')
+        self.assertEqual(str(item), 'Test todo item')
